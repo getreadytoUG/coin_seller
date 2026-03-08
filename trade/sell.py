9d62bf3ca3b7e912166628c26fb7cb146550c63a
junpyo <wnsvy1237@naver.com>
2026-03-08 12:25:32 +0900

    jwt

--- trade/sell.py
+++ trade/sell.py
@@ -53,28 +53,32 @@ def place_market_sell(access_key, secret_key, market, volume):
         "ord_type": "market",
         "volume": volume
     }
-    query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
     
-    m = hashlib.sha512()
-    m.update(query_string)
-    query_hash = m.hexdigest()
+    def make_jwt_token():
+        query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
+        
+        m = hashlib.sha512()
+        m.update(query_string)
+        query_hash = m.hexdigest()
 
-    payload = {
-        "access_key": access_key,
-        "nonce": str(uuid.uuid4()),
-        "query_hash": query_hash,
-        "query_hash_alg": "SHA512"
-    }
+        payload = {
+            "access_key": access_key,
+            "nonce": str(uuid.uuid4()),
+            "query_hash": query_hash,
+            "query_hash_alg": "SHA512"
+        }
 
-    jwt_token = jwt.encode(payload, secret_key, algorithm="HS256")
-    headers = {
-        "Authorization": f"Bearer {jwt_token}",
-        "Accept": "application/json"
-    }
+        jwt_token = jwt.encode(payload, secret_key, algorithm="HS256")
+        return jwt
     
     correct_flag = False
     for _ in range(3):
         try:
+            jwt_token, params = make_jwt_token()
+            headers = {
+                "Authorization": f"Bearer {jwt_token}",
+                "Accept": "application/json"
+            }
             response = requests.post(UPBIT_ORDER_URL, headers=headers, json=params)
             if response.status_code in (200, 201):
                 correct_flag = True
